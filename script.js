66746efd3c736cadd6f054fbd40b3c3ff0174a49
Maja Gedda <maja.gedda@zocom.se>
2020-10-20 15:26:21 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,15 @@
+//Toggle image when clicking on first button
+
+//the image: <img src="https://images.unsplash.com/photo-1432298026442-0eabd0a98870?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80" alt="image" width="100" height="100"></img>
+
+
+//On the second button a joke will appear underneath it
+
+
+//On the third button you should render a random number
+
+
+//The fourth button will add 1 + 1
+
+
+//The fifth button should change the header text to something else
